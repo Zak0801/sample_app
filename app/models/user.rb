2a63e30de87372b7a56029dfcd3eefed47f4b30e
Kazuki Shimizu <s.k24310723@gmail.com>
2020-05-26 20:28:04 +0900

    Modeling of account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
                     dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -70,4 +71,17 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成及び代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
